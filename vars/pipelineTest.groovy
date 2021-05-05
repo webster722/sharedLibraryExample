7a29e423f60fbf5c0d097fbbe76ee6ceec1c3195
webster722 <38156045+webster722@users.noreply.github.com>
2021-05-05 08:41:52 +0800

    Update pipelineTest.groovy

--- vars/pipelineTest.groovy
+++ vars/pipelineTest.groovy
@@ -1,20 +1,41 @@
 def call(body){
-    pipeline {
-        agent any
-        stages{
-            stage("Hello World"){
-                steps{
-                    script{
-                        print "Hello World"
+   pipeline{
+    agent any
+    stages{
+        stage('Clone'){
+            steps{
+                script{
+                    //git credentialsId: 'GithubCreds', url: 'https://github.com/webster722/DivyaTestRepo.git'
+                    withCredentials([usernamePassword(credentialsId: 'NewGitCreds', passwordVariable: 'pass', usernameVariable: 'user')]) {
+                        //git url: 'https://github.com/webster722/DivyaTestRepo.git'
+                        sh "git clone https://${user}:'${pass}'@github.com/webster722/DivyaTestRepo.git"
+                        dir('DivyaTestRepo'){
+                            sh "ls -lart"
+                            content = readYaml file: "./SampleYaml.yml"
+                            content.each{
+                                print "${it.Source}"
+                            }
+                        }
+                        sh "rm -rf ./*"
+                        try {
+                            print "this is ${NewValue}"
+                        }
+                        catch (exc) {
+                            print 'Something failed for variable'
+                        }
+
+                        
+                        
                     }
                 }
             }
         }
-        post{
-            always{
-                cleanWs()
-            }
+    }
+    post{
+        always{
+            cleanWs()
         }
     }
+}
     
 }
